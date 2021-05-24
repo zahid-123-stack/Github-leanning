debe083410deaf03922ea11a07677ab8ba40c84c
Zahid Mansur <zahidmansur32@gmail.com>
2021-05-24 05:54:24 +0530

    v-0.001

--- gs11/course/views.py
+++ gs11/course/views.py
@@ -3,7 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def learn_django(request):
-    return render(request,'course/course1.html')
+    cname ='Django'
+    duration = '4 months'
+    seats = 10
+    django_details = {'nm':cname,'du':duration,'st':seats}
+    # coursename = {'cname':'Zahid'}
+    return render(request,'course/course1.html',django_details)
 
 def learn_python(request):
     return render(request,'course/course2.html')
+    
